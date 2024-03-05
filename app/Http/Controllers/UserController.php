8fe3233f0d8516c4d902dd1dc00317c599252909
Alerakha <capupucino@gmail.com>
2024-03-05 09:57:50 +0700

    Praktikum 6 -sebelumnya

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,23 +3,36 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\View\View;
 
 class UserController extends Controller
 {
-    //
-    public function index(): View
+    // Pertemuan 2 ---
+    // public function index(): View
+    // {
+    //     // select
+    //     $users = DB::select('select * from m_user');
+    //     return view('user.index', ['users' => $users]);
+    //     // insert
+    //     DB::insert('insert into m_level(level_kode, level_nama) values(?,?)', ['Cus', 'Pelanggan']);
+    //     // update
+    //     DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'Cus']);
+    //     // delete
+    //     DB::delete('delete m_level where level_kode = ?', ['Cus']);
+    // }
+
+    public function index()
     {
-        // select
-        $users = DB::select('select * from m_user');
-        return view('user.index', ['users' => $users]);
-        // insert
-        DB::insert('insert into m_level(level_kode, level_nama) values(?,?)', ['Cus', 'Pelanggan']);
-        // update
-        DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'Cus']);
-        // delete
-        DB::delete('delete m_level where level_kode = ?', ['Cus']);
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data);
+
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
+use App\Models\UserModel;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,3 +22,4 @@ Route::get('/', function () {
 });
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
