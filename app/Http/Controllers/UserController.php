0aa52341c9d3485153dd7354fbbe1c95466af01c
Alerakha <capupucino@gmail.com>
2024-03-05 11:23:14 +0700

    praktikum 2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -42,10 +42,25 @@ class UserController extends Controller
     // }
 
     // praktikum 2
+    // public function index()
+    // {
+    //     $user = UserModel::where('level_id', 2)->count();
+    //     // dd($user);
+    //     return view('user', ['data' => $user]);
+    // }
+
+    // pratikum 3
     public function index()
     {
-        $user = UserModel::where('level_id', 2)->count();
-        // dd($user);
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->save();
         return view('user', ['data' => $user]);
     }
 }
