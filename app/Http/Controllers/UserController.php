01f2d48ec38c903dd65feb9ebf475cff9baf4c52
Alerakha <capupucino@gmail.com>
2024-03-05 10:31:16 +0700

    praktikum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -44,9 +44,7 @@ class UserController extends Controller
     // praktikum 2
     public function index()
     {
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
