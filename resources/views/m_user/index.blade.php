e4f9b4ecdaceb45fabf35478f89a46295d99a6be
Alerakha <capupucino@gmail.com>
2024-03-25 20:25:15 +0700

    Tugas 1

--- resources/views/m_user/index.blade.php
+++ resources/views/m_user/index.blade.php
@@ -1,11 +1,12 @@
 @extends('m_user/template')
+@extends('adminlte::page')
 @section('content')
 <div class="row mt-5 mb-5">
-    <div class="col-lg-12 margin-tb">
+    <div class="col-lg-12 margin-tb bg-dark rounded-2">
         <div class="float-left">
-            <h2>CRUD user</h2>
+            <h2 class="text-light">CRUD user</h2>
         </div>
-        <div class="float-right">
+        <div class="float-right m-2">
             <a href="{{route('m_user.create') }}" class="btn btn-success">Input User</a>
         </div>
     </div>
@@ -17,11 +18,11 @@
 @endif
 <table class="table table-bordered">
 <tr>
-<th width="20px" class="text-center">User id</th>
-<th width="150px" class="text-center">Level id</th>
-<th width="200px"class="text-center">username</th>
-<th width="200px"class="text-center">nama</th>
-<th width="150px"class="text-center">password</th>
+<th width="20px" class="text-center ">User id</th>
+<th width="150px" class="text-center ">Level id</th>
+<th width="200px"class="text-center ">Username</th>
+<th width="200px"class="text-center ">Nama</th>
+<th width="150px"class="text-center ">Password</th>
 </tr>
 @foreach ($useri as $m_user)
 <tr>
@@ -31,13 +32,13 @@
 <td>{{ $m_user->nama }}</td>
 <td>{{ $m_user->password }}</td>
 <td class="text-center">
-<form action="{{ route('m_user.destroy',$m_user->user_id) }}"method="POST">
-<a class="btn btn-info btn-sm" href="{{ route('m_user.show',$m_user->user_id) }}">Show</a>
-<a class="btn btn-primary btn-sm" href="{{
+<form class="d-flex" action="{{ route('m_user.destroy', $m_user->user_id) }}"method="POST">
+<a class="btn btn-info btn-sm mr-2 m-1" href="{{ route('m_user.show',$m_user->user_id) }}">Show</a>
+<a class="btn btn-primary btn-sm mr-2 m-1" href="{{
 route('m_user.edit',$m_user->user_id) }}">Edit</a>
 @csrf
 @method('DELETE')
-<button type="submit" class="btn btn-danger btn-sm" onclick="return
+<button type="submit" class="btn btn-danger btn-sm mr-2 m-1" onclick="return
 confirm('Apakah Anda yakin ingin menghapus data ini?')">Delete</button>
 </form>
 </td>
