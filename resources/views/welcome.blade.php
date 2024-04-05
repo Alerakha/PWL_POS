da2cc0f03324136ba03138ec95dc727ba9f4a055
Alerakha <capupucino@gmail.com>
2024-04-05 17:28:53 +0700

    TUGAS Jobsheet7

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,13 +2,23 @@
 
 @section('content')
 
-<div class="card">
-    <div class="card-header">
-        <h3 class="card-title">Hello, How are you!!</h3>
-        <div class="card-tools"></div>
-    </div>
-    <div class="card-body">
-        Welcome all, this is main page for this application
+<div class="jumbotron jumbotron-fluid text-center bg-dark">
+    <div class="container text-white">
+        <h1 class="display-4"><strong>Welcome to PWL_POS Jobsheet7!</strong></h1>
+        <p class="lead">Explore, Connect, and Enjoy</p>
     </div>
 </div>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header bg-dark text-white">
+                    <h3 class="card-title">Hello, how are you! :D</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
