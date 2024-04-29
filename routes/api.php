ac5db5c803cb2c863f3fddd4117a1ff7b23e4dcd
Alerakha <capupucino@gmail.com>
2024-04-29 22:05:12 +0700

    Praktikum 4

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
 use App\Http\Controllers\Api\RegisterController;
+// use App\Http\Controllers\LevelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\LevelController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -25,3 +26,10 @@ Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->na
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// route level
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
